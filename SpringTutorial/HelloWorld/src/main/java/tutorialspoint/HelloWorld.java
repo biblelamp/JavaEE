69168271b2c4e841673f424e644bd50b9e46e8ac
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-20 11:22:04 +0300

    hello world in Spring from TutorialPoint

---
+++ SpringTutorial/HelloWorld/src/main/java/tutorialspoint/HelloWorld.java
@@ -0,0 +1,12 @@
+package tutorialspoint;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println(message);
+    }
+}
