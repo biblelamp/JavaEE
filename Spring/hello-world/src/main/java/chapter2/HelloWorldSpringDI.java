c0ee1fb6e8020d348da77e553f587a7b4f3e1cf6
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-20 09:12:47 +0300

    hello world Spring DI

---
+++ Spring/hello-world/src/main/java/chapter2/HelloWorldSpringDI.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloWorldSpringDI {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext
+           ("META-INF/spring/app-context.xml");
+
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
