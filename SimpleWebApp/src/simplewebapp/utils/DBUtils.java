7523a039e89432b1ee34a4d57f22624ed815d217
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-24 17:19:30 +0300

    fixed sql queries

--- SimpleWebApp/src/simplewebapp/utils/DBUtils.java
+++ SimpleWebApp/src/simplewebapp/utils/DBUtils.java
@@ -6,25 +6,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import simplewebapp.beans.Product;
 import simplewebapp.beans.UserAccount;
 
 public class DBUtils {
-	 
+
 	public static UserAccount findUser(Connection conn, String userName,
 			String password) throws SQLException {
 
-		String sql = "Select a.User_Name, a.Password, a.Gender from " +
-				"User_Account a where a.User_Name = ? and a.password= ?";
+		String sql = "SELECT user_name, gender, password FROM " +
+	        "user_account WHERE user_name = ? and password = ?";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
         pstm.setString(1, userName);
         pstm.setString(2, password);
         ResultSet rs = pstm.executeQuery();
- 
+
         if (rs.next()) {
-            String gender = rs.getString("Gender");
+            String gender = rs.getString("gender");
             UserAccount user = new UserAccount();
             user.setUserName(userName);
             user.setPassword(password);
@@ -37,16 +37,16 @@ public class DBUtils {
 	public static UserAccount findUser(Connection conn, String userName)
 			throws SQLException {
 
-		String sql = "Select a.User_Name, a.Password, a.Gender from " +
-		"User_Account a where a.User_Name = ? ";
- 
+		String sql = "SELECT user_name, gender, password FROM " +
+	        "user_account WHERE user_name = ?";
+
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		pstm.setString(1, userName);
 		ResultSet rs = pstm.executeQuery();
 
 		if (rs.next()) {
-			String password = rs.getString("Password");
-			String gender = rs.getString("Gender");
+			String password = rs.getString("password");
+			String gender = rs.getString("gender");
 			UserAccount user = new UserAccount();
 			user.setUserName(userName);
 			user.setPassword(password);
@@ -59,15 +59,15 @@ public class DBUtils {
 	public static List<Product> queryProduct(Connection conn)
 			throws SQLException {
 
-		String sql = "Select a.Code, a.Name, a.Price from Product a ";
+		String sql = "SELECT code, name, price FROM product";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		ResultSet rs = pstm.executeQuery();
 		List<Product> list = new ArrayList<Product>();
 		while (rs.next()) {
-			String code = rs.getString("Code");
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
+			String code = rs.getString("code");
+			String name = rs.getString("name");
+			float price = rs.getFloat("price");
 			Product product = new Product();
 			product.setCode(code);
 			product.setName(name);
@@ -80,15 +80,15 @@ public class DBUtils {
 	public static Product findProduct(Connection conn, String code)
 			throws SQLException {
 
-		String sql = "Select a.Code, a.Name, a.Price from Product a where a.Code=?";
+		String sql = "SELECT code, name, price FROM product WHERE code = ?";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		pstm.setString(1, code);
 		ResultSet rs = pstm.executeQuery();
 
 		while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
+			String name = rs.getString("name");
+			float price = rs.getFloat("price");
 			Product product = new Product(code, name, price);
 			return product;
 		}
@@ -98,7 +98,7 @@ public class DBUtils {
 	public static void updateProduct(Connection conn, Product product)
 			throws SQLException {
 
-		String sql = "Update Product set Name =?, Price=? where Code=? ";
+		String sql = "UPDATE product set name = ?, price = ? WHERE code = ?";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		pstm.setString(1, product.getName());
@@ -110,7 +110,7 @@ public class DBUtils {
 	public static void insertProduct(Connection conn, Product product)
 			throws SQLException {
 
-		String sql = "Insert into Product(Code, Name,Price) values (?,?,?)";
+		String sql = "INSERT INTO product(code, name, price) VALUES (?,?,?)";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		pstm.setString(1, product.getCode());
@@ -122,7 +122,7 @@ public class DBUtils {
 	public static void deleteProduct(Connection conn, String code)
 			throws SQLException {
 
-		String sql = "Delete Product where Code= ?";
+		String sql = "DELETE product WHERE code = ?";
 
 		PreparedStatement pstm = conn.prepareStatement(sql);
 		pstm.setString(1, code);
