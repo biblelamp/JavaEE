cbeb4c97e225dd11daa4262e258949b176e8df76
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-24 17:27:27 +0300

    mysql connection fixed a little

--- SimpleWebApp/src/simplewebapp/conn/MySQLConnUtils.java
+++ SimpleWebApp/src/simplewebapp/conn/MySQLConnUtils.java
@@ -11,7 +11,7 @@ public class MySQLConnUtils {
 
 	    // Note: Change the connection parameters accordingly.
 	    String hostName = "localhost";
-	    String dbName = "simplewa";
+	    String dbName = "simple_web_app";
 	    String userName = "root";
 	    String password = "root";
 	    return getMySQLConnection(hostName, dbName, userName, password);
@@ -20,18 +20,17 @@ public class MySQLConnUtils {
 	public static Connection getMySQLConnection(String hostName, String dbName,
 	        String userName, String password) throws SQLException,
 	        ClassNotFoundException {
-	    
+
 	    // Declare the class Driver for MySQL DB
 	    // This is necessary with Java 5 (or older)
 	    // Java6 (or newer) automatically find the appropriate driver.
 	    // If you use Java> 5, then this line is not needed.
 	    Class.forName("com.mysql.jdbc.Driver");
-	 
-	 
+
 	    // URL Connection for MySQL
-	    // Example: jdbc:mysql://localhost:3306/simplewa
+	    // Example: jdbc:mysql://localhost:3306/simple_web_app
 	    String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-	 
+
 	    Connection conn = DriverManager.getConnection(connectionURL, userName,
 	            password);
 	    return conn;
