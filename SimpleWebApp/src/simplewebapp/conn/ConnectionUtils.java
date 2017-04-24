cd3dafa4e48b7be0ebf050373176a26efb73acc5
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-24 09:05:33 +0300

    database connection utility classes

---
+++ SimpleWebApp/src/simplewebapp/conn/ConnectionUtils.java
@@ -0,0 +1,31 @@
+package simplewebapp.conn;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+
+	public static Connection getConnection()
+			throws ClassNotFoundException, SQLException {
+	      
+		// I'm using MySQL Database
+		return MySQLConnUtils.getMySQLConnection();
+		//return OracleConnUtils.getOracleConnection();
+	    //return SQLServerConnUtils_JTDS.getSQLServerConnection_JTDS();
+		//return SQLServerConnUtils_SQLJDBC.getSQLServerConnection_SQLJDBC();
+	}
+
+	public static void closeQuietly(Connection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+		}
+	}
+
+	public static void rollbackQuietly(Connection conn) {
+		try {
+			conn.rollback();
+		} catch (Exception e) {
+		}
+	}
+}
