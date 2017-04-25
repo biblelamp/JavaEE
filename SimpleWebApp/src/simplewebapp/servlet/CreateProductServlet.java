2c78967d2459792890e13606190324bf5f9b690d
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-25 11:56:11 +0300

    create product jsp and servlets

---
+++ SimpleWebApp/src/simplewebapp/servlet/CreateProductServlet.java
@@ -0,0 +1,34 @@
+package simplewebapp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/createProduct"})
+public class CreateProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CreateProductServlet() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, 
+	    HttpServletResponse response) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = request.getServletContext()
+              .getRequestDispatcher("/WEB-INF/views/createProductView.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+  }
+}
