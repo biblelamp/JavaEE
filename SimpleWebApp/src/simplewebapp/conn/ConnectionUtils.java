49fb771b979531b0af558440d0d638fe82f5808d
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-25 10:43:13 +0300

    switch to mySQL database

--- SimpleWebApp/src/simplewebapp/conn/ConnectionUtils.java
+++ SimpleWebApp/src/simplewebapp/conn/ConnectionUtils.java
@@ -8,9 +8,9 @@ public class ConnectionUtils {
 	public static Connection getConnection()
 			throws ClassNotFoundException, SQLException {
 
-		// I'm using SQLite Database
-		return SQLiteConnUtils.getSQLiteConnection();
-		//return MySQLConnUtils.getMySQLConnection();
+		// I'm using MySQL Database
+		return MySQLConnUtils.getMySQLConnection();
+		//return SQLiteConnUtils.getSQLiteConnection();
 		//return OracleConnUtils.getOracleConnection();
 	    //return SQLServerConnUtils_JTDS.getSQLServerConnection_JTDS();
 		//return SQLServerConnUtils_SQLJDBC.getSQLServerConnection_SQLJDBC();
