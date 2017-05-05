0d8a7cfe2e221bcb61f82687572f4815661c8fbb
Sergey Iryupin <biblelamp@yandex.ru>
2017-05-05 16:18:29 +0300

    hello eclipse added hello servlet

---
+++ Webinars/HelloEclipse/src/hello/HelloServlet.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.io.*;
+import javax.servlet.*;
+
+public class HelloServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<!DOCTYPE html>\n"+
+            "<html>\n"+
+            "<head>\n"+
+            "<meta charset='UTF-8'>\n"+
+            "<title>Hello Servlet</title>\n"+
+            "</head>\n"+
+            "<body>\n"+
+            "<h2>Hello Servlet!</h2>\n"+
+            "Go <a href='"+
+            	request.getServletContext().getContextPath()+
+            "'>Back</a>\n"+
+            "</body>\n"+
+            "</html>");
+        pw.close();
+    }
+}
