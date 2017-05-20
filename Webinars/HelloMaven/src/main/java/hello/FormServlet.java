c4b224198c967c4b54d29e548fa8035805a3219a
Sergey Iryupin <biblelamp@yandex.ru>
2017-05-20 23:15:57 +0300

    using form page in html

--- Webinars/HelloMaven/src/main/java/hello/FormServlet.java
+++ Webinars/HelloMaven/src/main/java/hello/FormServlet.java
@@ -8,89 +8,56 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
+        HttpServletResponse response) throws ServletException, IOException {
 
-		// get data from request
-		String code = request.getParameter("code");
-		String name = request.getParameter("name");
-		String price = request.getParameter("price");
+        // get data from request
+        String code = request.getParameter("code");
+        String name = request.getParameter("name");
+        String price = request.getParameter("price");
 
-		// prepared to write as html
-		response.setContentType("text/html");
-		PrintWriter pw = response.getWriter();
-			
-		if (code == null || name == null || price == null) {
-			// no data
-			pw.println("<!DOCTYPE html>\n"+
-			"<html>\n"+
-			"<head>\n"+
-			"<meta charset='UTF-8'>\n"+
-			"<title>GetForm Servlet</title>\n"+
-			"</head>\n"+
-			"<body>\n"+
-			"<h2>GetForm Servlet:</h2>\n"+
-			"<form method='POST' action='FormServlet'>\n"+
-			"<table border='0'>\n"+
-			"<tr>\n"+
-			"<td>Code</td>\n"+
-			"<td><input type='text' name='code'/></td>\n"+
-			"</tr>\n"+
-			"<tr>\n"+
-			"<td>Name</td>\n"+
-			"<td><input type='text' name='name'/></td>\n"+
-			"</tr>\n"+
-			"<tr>\n"+
-			"<td>Price</td>\n"+
-			"<td><input type='text' name='price'/></td>\n"+
-			"</tr>\n"+
-			"<tr>\n"+
-			"<td colspan='2'>\n"+           
-			"<input type='submit' value='Submit'/>\n"+
-			"</td>\n"+
-			"</tr>\n"+
-			"</table>\n"+
-			"</form>\n"+
-			"Go <a href='"+
-				request.getServletContext().getContextPath()+
-			"'>Back</a>\n"+
-			"</body>\n"+
-			"</html>");
-		} else {
-			// have data
-			pw.println("<!DOCTYPE html>\n"+
-			"<html>\n"+
-			"<head>\n"+
-			"<meta charset='UTF-8'>\n"+
-			"<title>GetForm Servlet</title>\n"+
-			"</head>\n"+
-			"<body>\n"+
-			"<h2>GetForm Servlet:</h2>\n"+
-			"<table border='0'>\n"+
-			"<tr>\n"+
-			"<td>Code</td>\n"+
-			"<td><b>" + code + "</b></td>\n"+
-			"</tr>\n"+
-			"<tr>\n"+
-			"<td>Name</td>\n"+
-			"<td><b>" + name + "</b></td>\n"+
-			"</tr>\n"+
-			"<tr>\n"+
-			"<td>Price</td>\n"+
-			"<td><b>" + price + "</b></td>\n"+
-			"</tr>\n"+
-			"</table>\n"+
-			"Go <a href='"+
-				request.getServletContext().getContextPath()+
-			"'>Back</a>\n"+
-			"</body>\n"+
-			"</html>");
-		}
-		pw.close();
+        if (code == null || name == null || price == null) {
+            // if no data
+            RequestDispatcher dispatcher = request.getServletContext()
+                .getRequestDispatcher("/form.html");
+            dispatcher.forward(request, response);
+        } else {
+            // have data
+            response.setContentType("text/html");
+            PrintWriter pw = response.getWriter();
+            pw.println("<!DOCTYPE html>\n"+
+            "<html>\n"+
+            "<head>\n"+
+            "<meta charset='UTF-8'>\n"+
+            "<title>GetForm Servlet</title>\n"+
+            "</head>\n"+
+            "<body>\n"+
+            "<h2>GetForm Servlet:</h2>\n"+
+            "<table border='0'>\n"+
+            "<tr>\n"+
+            "<td>Code</td>\n"+
+            "<td><b>" + code + "</b></td>\n"+
+            "</tr>\n"+
+            "<tr>\n"+
+            "<td>Name</td>\n"+
+            "<td><b>" + name + "</b></td>\n"+
+            "</tr>\n"+
+            "<tr>\n"+
+            "<td>Price</td>\n"+
+            "<td><b>" + price + "</b></td>\n"+
+            "</tr>\n"+
+            "</table>\n"+
+            "Go <a href='"+
+                request.getServletContext().getContextPath()+
+            "'>Back</a>\n"+
+            "</body>\n"+
+            "</html>");
+            pw.close();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
+        HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
-	}
+    }
 }
