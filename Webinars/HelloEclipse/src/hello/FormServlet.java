1bd8016c992f4bc0bf21ee78ebc4533656a6789e
Sergey Iryupin <biblelamp@yandex.ru>
2017-05-25 15:54:12 +0300

    improved form servlet code

--- Webinars/HelloEclipse/src/hello/FormServlet.java
+++ Webinars/HelloEclipse/src/hello/FormServlet.java
@@ -1,7 +1,6 @@
 package hello;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -16,49 +15,23 @@ public class FormServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
 
+        String page; // page for forwarding
+
         // get data from request
         String code = request.getParameter("code");
         String name = request.getParameter("name");
         String price = request.getParameter("price");
 
-        if (code == null || name == null || price == null) {
-            // if no data
-            RequestDispatcher dispatcher = request.getServletContext()
-                .getRequestDispatcher("/form.html");
-            dispatcher.forward(request, response);
-        } else {
-            // have data - show data
-            response.setContentType("text/html");
-            PrintWriter pw = response.getWriter();
-            pw.println("<!DOCTYPE html>\n"+
-            "<html>\n"+
-            "<head>\n"+
-            "<meta charset='UTF-8'>\n"+
-            "<title>GetForm Servlet</title>\n"+
-            "</head>\n"+
-            "<body>\n"+
-            "<h2>GetForm Servlet:</h2>\n"+
-            "<table border='0'>\n"+
-            "<tr>\n"+
-            "<td>Code</td>\n"+
-            "<td><b>" + code + "</b></td>\n"+
-            "</tr>\n"+
-            "<tr>\n"+
-            "<td>Name</td>\n"+
-            "<td><b>" + name + "</b></td>\n"+
-            "</tr>\n"+
-            "<tr>\n"+
-            "<td>Price</td>\n"+
-            "<td><b>" + price + "</b></td>\n"+
-            "</tr>\n"+
-            "</table>\n"+
-            "Go <a href='"+
-                request.getServletContext().getContextPath()+
-            "'>Back</a>\n"+
-            "</body>\n"+
-            "</html>");
-            pw.close();
-        }
+        if (code == null || name == null || price == null)
+            page = "/form.html"; // if no data
+        else
+            page = "/form.jsp"; // if have data
+
+        // get the RD object for redirecting
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(page);
+
+        // redirect to page
+        rd.forward(request, response);
     }
 
 	protected void doPost(HttpServletRequest request,
