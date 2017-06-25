0a9e26fe8651e1e0feefede321065db63b575601
Sergey Iryupin <biblelamp@yandex.ru>
2017-06-25 13:31:26 +0300

    change main class for using appconfig class

--- SpringGB/lesson1/camera_eclipse/src/main/java/spring/Client.java
+++ SpringGB/lesson1/camera_eclipse/src/main/java/spring/Client.java
@@ -1,7 +1,8 @@
 package spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 
@@ -10,12 +11,12 @@ public class Client {
         System.out.println("It's a camera.");
 
         // Remove the comment of two lines below for XML configuring
-        ApplicationContext context = new ClassPathXmlApplicationContext
-            ("config.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext
+        //    ("config.xml");
 
         // Remove the comment of two lines below for JavaConfig
-        //ApplicationContext context =
-        //  new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context =
+        		new AnnotationConfigApplicationContext(AppConfig.class);
 
         Camera camera = context.getBean("camera", Camera.class);
         camera.doPhotograph();
