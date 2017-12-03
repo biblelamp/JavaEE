4b7bba9d3854ead28283ef86e29189e0d2f12395
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-03 14:52:41 +0300

    deleted file from old directory

--- SpringGB/lesson1/camera/src/main/java/spring/ColorCameraRoll.java
+++
@@ -1,9 +0,0 @@
-package spring;
-
-public class ColorCameraRoll implements CameraRoll {
-
-    @Override
-    public void processing() {
-        System.out.println("-1 color photo");
-    }
-}
--- SpringGB/lesson1/rifle/src/main/java/spring/Rifle.java
+++
@@ -1,7 +0,0 @@
-package spring;
-
-public interface Rifle {
-    Bullets getBullets();
-    void setBullets(Bullets bullets);
-    void shot();
-}
