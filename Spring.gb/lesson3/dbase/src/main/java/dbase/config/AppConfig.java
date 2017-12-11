6f6e9021074d3e6c7aaab12335b995c05d7242d4
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-11 11:48:01 +0300

    lesson 3 init code

---
+++ Spring.gb/lesson3/dbase/src/main/java/dbase/config/AppConfig.java
@@ -0,0 +1,74 @@
+package dbase.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name="dataSource")
+    public DataSource getDataSource(){
+
+        // источник данных
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        // параметры подключения к базе данных
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:mysqlite.db");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean(name="entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean getEntityManager(){
+        // класса фабрики реализующей интерфейс FactoryBean<EntityManagerFactory>
+        LocalContainerEntityManagerFactoryBean factory =
+            new LocalContainerEntityManagerFactoryBean();
+
+        // источник подключения
+        factory.setDataSource(getDataSource());
+
+        // адаптера для конкретной реализации JPA, 
+        // указывает, какая библиотека будет использоваться в качестве постовщика
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        // указание пакета в котором находятся классы-сущности
+        factory.setPackagesToScan("dbase.domain");
+
+        //создание свойств для настройки Hibernate
+        Properties jpaProperties = new Properties();
+
+        // диалект базы данных,необходимо для генерации запросов Hibernate к БД
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect");
+
+        // максимальная глубины связи(будет рассмотрено в след. уроке)
+        jpaProperties.put("hibernate.max_fetch_depth", 3);
+
+        // максимальное число строк, возвращаемых за один запрос из БД
+        jpaProperties.put("hibernate.jdbc.fetch_size", 50);
+
+        // максимальное число запросов при использовании пакетных операций
+        jpaProperties.put("hibernate.jdbc.batch_size", 10);
+
+        // включает логгирование
+        jpaProperties.put("hibernate.show_sql", true);
+
+        factory.setJpaProperties(jpaProperties);
+        return factory;
+    }
+}
---
+++ Spring.gb/lesson3/dbase/src/main/java/dbase/domain/Author.java
@@ -0,0 +1,73 @@
+package dbase.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstname;
+
+    @Column(name="last_name")
+    private String lastname;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToMany(mappedBy="author")
+    private List<Article> articles;
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
