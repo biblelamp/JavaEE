2b676b81bd640d28961e3a26f4a1ae0c45df612c
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-11 21:15:18 +0300

    deleted unused import

--- Spring.gb/lesson3/dbase/src/main/java/dbase/config/AppConfig.java
+++ Spring.gb/lesson3/dbase/src/main/java/dbase/config/AppConfig.java
@@ -1,21 +1,14 @@
 package dbase.config;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import java.util.Properties;
 
 @Configuration
 public class AppConfig {
