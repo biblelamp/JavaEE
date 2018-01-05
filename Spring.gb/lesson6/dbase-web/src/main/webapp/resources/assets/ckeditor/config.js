70b732da2993d18ec00178318a5fa87d0ade8581
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-05 04:21:21 +0300

    changed toolbar of ckeditor

--- Spring.gb/lesson6/dbase-web/src/main/webapp/resources/assets/ckeditor/config.js
+++ Spring.gb/lesson6/dbase-web/src/main/webapp/resources/assets/ckeditor/config.js
@@ -10,24 +10,24 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for a single toolbar row.
 	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
+		{ name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms',       groups: [ 'forms' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links',       groups: [ 'links' ] },
+		{ name: 'insert',      groups: [ 'insert' ] },
+		{ name: 'styles',      groups: [ 'styles' ] },
+		{ name: 'colors',      groups: [ 'colors' ] },
+		{ name: 'tools',       groups: [ 'tools' ] },
+		{ name: 'others',      groups: [ 'others' ] },
+		{ name: 'about',       groups: [ 'about' ] }
 	];
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	config.removeButtons = 'Cut,Copy,Paste,Redo,Anchor,Strike,Subscript,Superscript';
 
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = 'link:advanced';
