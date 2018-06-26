8789dd854cec686d7a9fc6e86840f2c3e4364b37
Sergey Iryupin <biblelamp@yandex.ru>
2018-06-26 08:39:07 +0200

    init commit

---
+++ SpringREST/src/main/java/rest/spring/config/WebConfig.java
@@ -0,0 +1,12 @@
+package rest.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"rest.spring.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
