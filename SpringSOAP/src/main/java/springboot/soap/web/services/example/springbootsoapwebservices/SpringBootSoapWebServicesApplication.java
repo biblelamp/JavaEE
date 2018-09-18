2d90059ddbd86a1d0d475dd414a9fc9c378f73e0
Sergey Iryupin <biblelamp@yandex.ru>
2018-09-18 14:48:04 +0200

    init commit

---
+++ SpringSOAP/src/main/java/springboot/soap/web/services/example/springbootsoapwebservices/SpringBootSoapWebServicesApplication.java
@@ -0,0 +1,12 @@
+package springboot.soap.web.services.example.springbootsoapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWebServicesApplication.class, args);
+	}
+}
---
+++ SpringSOAP/src/test/java/springboot/soap/web/services/example/springbootsoapwebservices/SpringBootSoapWebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package springboot.soap.web.services.example.springbootsoapwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSoapWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
