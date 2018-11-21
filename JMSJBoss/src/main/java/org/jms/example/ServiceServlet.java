300021c42641dc15cb81e62f3370211ef09381cf
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-21 15:18:56 +0100

    added read operation

--- JMSJBoss/src/main/java/org/jms/example/ServiceServlet.java
+++ JMSJBoss/src/main/java/org/jms/example/ServiceServlet.java
@@ -2,7 +2,8 @@ package org.jms.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
- 
+import java.util.*;
+
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,27 +16,37 @@ public class ServiceServlet extends HttpServlet {
 	@Inject
 	Sender sender;
 
+	@Inject
+	Reader reader;
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) 
     		                 throws ServletException, IOException {
 		String mode   = request.getParameter("mode"  );
 		String prefix = request.getParameter("prefix");
 
-		if(prefix == null || "".equals(prefix))
+		if (prefix == null || "".equals(prefix))
 			prefix = "prefix";
     	
     	response.setContentType("text/html;charset=UTF-8");
     	PrintWriter out = response.getWriter();
 		if (mode.equalsIgnoreCase("send")) {
-	    	out.print("send messages :<br>");
+	    	out.print("send messages :<br />");
 	    	for (int i = 0; i < 3; i++) {
 	    		String msg = prefix + " #" + i;
 	    		out.print("&bull; " + msg + "<br />");
 	    		sender.sendMessage(msg);
 	    	}
 	    	out.close();
+		} if (mode.equalsIgnoreCase("list")) {
+			out.print("list messages :<br />");
+			List<String> messages = reader.readMessages("java:/jms/queue/ExpiryQueue");
+			for (String str : messages) {
+				out.print("&mdash; " + str + "<br />");
+			}
+			out.close();
 		} else {
-	    	out.print("receive messages :<br>");
+	    	out.print("receive messages :<br />");
 	    	if (Receiver.messages.size() > 0) {
 	    		for (int i = 0; i < Receiver.messages.size(); i++) {
 		    		out.print("&mdash; " + Receiver.messages.get(i) + "<br />");
