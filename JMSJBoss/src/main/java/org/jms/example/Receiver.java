019625f4e616bd104731c6e6afff4a8100038aad
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-22 08:03:49 +0100

    added simulating fail

--- JMSJBoss/src/main/java/org/jms/example/Receiver.java
+++ JMSJBoss/src/main/java/org/jms/example/Receiver.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
- 
+
+import javax.annotation.*;
 import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
 import javax.ejb.ActivationConfigProperty;
 import javax.jms.Message;
 import javax.jms.JMSException;
@@ -24,11 +26,17 @@ public class Receiver implements MessageListener {
 
 	public static List<String> messages = new ArrayList<>();
 
+	@Resource
+	private MessageDrivenContext mdc;
+
 	public void onMessage(Message rcvMessage) {
 		try {
 			messages.add(rcvMessage.getBody(String.class));
 
 			LOG.log(Level.INFO, "Received: " + rcvMessage.getBody(String.class));
+
+			mdc.setRollbackOnly(); // simulate fail
+
 		} catch (JMSException ex) {
 			LOG.log(Level.SEVERE, ex.getMessage(), ex);
 		}
