fe164232eaa38d515bb6e2463af8978c266538a2
Sergey Iryupin <biblelamp@yandex.ru>
2018-12-05 12:18:54 +0100

    added post method

--- RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
+++ RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
@@ -1,19 +1,28 @@
 package cz.tendersystems;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/publish")
+@Path("/service")
 public class RestEasyExample {
 
 	@GET
 	@Path("/{message}")
-	public Response publishMessage(@PathParam("message") String msgStr) {
+	public Response getMessage(@PathParam("message") String message) {
 
-		String responseStr = "Received message: " + msgStr;
+		String responseStr = "Received message: " + message + "\n";
 
 		return Response.status(200).entity(responseStr).build();
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes("application/json")
+	public Response postMessage(String message) {
+		return Response.status(200).entity(message).build();
 	}
 }
