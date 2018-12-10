af1edd968be808643535409eaad5f82e38ca5fb3
Sergey Iryupin <biblelamp@yandex.ru>
2018-12-10 15:42:03 +0100

    using application/json for reading file

--- RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
+++ RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
@@ -1,5 +1,7 @@
 package cz.tendersystems;
 
+import org.json.*;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Consumes;
@@ -7,6 +9,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import java.io.*;
+import java.util.*;
 import java.util.logging.*;
 
 @Path("/service")
@@ -25,10 +29,40 @@ public class RestEasyExample {
 
 	@POST
 	@Path("/post")
-	@Consumes("text/plain")
+	@Consumes("application/json")
 	public Response postMessage(String message) {
 
-		LOG.log(Level.INFO, message);
+		JSONObject json = new JSONObject(message);
+		JSONArray body = json.getJSONArray("body");
+		byte[] buffer = new byte[body.length()];
+		int i = 0;
+
+		LOG.log(Level.INFO, body.toString());
+		LOG.log(Level.INFO, Integer.toString(body.length()));
+
+		try {
+
+			File file = new File("/home/lamp/Downloads/Ahoj jak se máš.encrypt");
+			byte[] buf = new byte[(int) file.length()];
+			BufferedInputStream bis =
+					new BufferedInputStream(new FileInputStream(file));
+			bis.read(buf);
+			bis.close();
+
+			for (i = 0; i < buffer.length; i++) {
+				buffer[i] = ((Integer) body.get(i)).byteValue();
+
+				if (buffer[i] != buf[i]) {
+					LOG.log(Level.INFO, String.format("buffer[ %d ] = %d != %d", i, buf[i], buffer[i]));
+				}
+			}
+
+			Decrypt.decryptFile(buffer, json.getString("fileName"));
+
+		} catch (Exception ex) {
+			LOG.log(Level.INFO, String.format("buffer[ %d ] = %d", i, buffer[i]));
+			ex.printStackTrace();
+		}
 
 		return Response.status(200).entity(message).build();
 	}
