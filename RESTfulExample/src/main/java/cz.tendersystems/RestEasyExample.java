96d5aa251171a24b79f44518a828aa64b90d1d1a
Sergey Iryupin <biblelamp@yandex.ru>
2018-12-13 08:47:04 +0100

    added work with header

--- RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
+++ RESTfulExample/src/main/java/cz.tendersystems/RestEasyExample.java
@@ -5,6 +5,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -29,25 +30,27 @@ public class RestEasyExample {
 	@POST
 	@Path("/post")
 	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
-	public Response getData(InputStream inputStream) {
+	public Response getData(InputStream inputStream,
+			@HeaderParam("Content-Length") int contentLength,
+			@HeaderParam("Title") String fileName) {
+
+		LOG.log(Level.INFO, Integer.toString(contentLength));
+		LOG.log(Level.INFO, fileName);
 
 		try {
 
-			byte[] buffer = new byte[514];
+			byte[] buffer = new byte[contentLength];
 			inputStream.read(buffer);
 			inputStream.close();
 
-			LOG.log(Level.INFO, Arrays.toString(buffer));
+			// LOG.log(Level.INFO, Arrays.toString(buffer));
 
-			// byte[] file = Decrypt.readFile("/home/lamp/Downloads/Ahoj jak se máš.encrypt");
-			// LOG.log(Level.INFO, Arrays.toString(file));
-
-			Decrypt.decryptFile(buffer, "123456789");
+			Decrypt.decryptFile(buffer, Decrypt.PATH + fileName);
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
-		return Response.status(200).entity("123456789").build();
+		return Response.status(200).entity(fileName).build();
 	}
 }
