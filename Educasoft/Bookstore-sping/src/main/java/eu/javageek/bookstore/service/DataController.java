5e7427343ceda23e9f62226416b1db8d2bb46ec6
Sergey Iryupin <biblelamp@yandex.ru>
2019-01-25 15:31:30 +0100

    changed add method

--- Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/service/DataController.java
+++ Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/service/DataController.java
@@ -6,21 +6,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import eu.javageek.bookstore.domain.Author;
 import eu.javageek.bookstore.domain.Book;
 import eu.javageek.bookstore.repositories.BookRepository;
+import eu.javageek.bookstore.repositories.AuthorRepository;
 
 @Controller
 public class DataController {
 
 	@Autowired
 	BookRepository bookRepository;
+	
+	@Autowired
+	AuthorRepository authorRepository;
 
 	@GetMapping(path="/add")
-	public @ResponseBody String addNewBook(@RequestParam String name) {
+	public @ResponseBody String addNewBook(@RequestParam String name, 
+			@RequestParam String author) {
+
+		Author authorOfBook;
+
+		if (author.chars().allMatch(Character::isDigit)) {
+			authorOfBook = authorRepository.findOne(Integer.parseInt(author));
+		} else {
+			authorOfBook = new Author();
+			authorOfBook.setName(author);
+			authorRepository.save(authorOfBook);
+		}
+
 		Book book = new Book();
 		book.setName(name);
+		book.setAuthorOfBook(authorOfBook);
 		bookRepository.save(book);
+
 		return "Added";
+	}
+
+	@GetMapping(path="/delete")
+	public @ResponseBody String deleteBookById(@RequestParam Integer id) {
+		if (bookRepository.exists(id)) {
+			bookRepository.delete(id);
+			return "Deleted";
+		}
+		return "Not found";
+	}
+
+	@GetMapping(path="/deleteAll")
+	public @ResponseBody String deleteAll() {
+		bookRepository.deleteAll();
+		return "Deleted all";
 	}
 
 	@GetMapping(path="/all")
