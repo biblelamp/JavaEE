cdce7b1be374cfdd56df2e65740670902440b182
Sergey Iryupin <biblelamp@yandex.ru>
2019-01-25 11:37:11 +0100

    init commit

---
+++ Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/service/IndexController.java
@@ -0,0 +1,14 @@
+package eu.javageek.bookstore.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings in Bookstore Application!";
+    }
+
+}
