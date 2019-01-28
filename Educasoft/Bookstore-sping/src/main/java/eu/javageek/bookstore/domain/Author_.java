df5b0fab1dbb4abe07526a5ec4bb57697221061d
Sergey Iryupin <biblelamp@yandex.ru>
2019-01-28 15:20:37 +0100

    added abstract classes

---
+++ Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/domain/Author_.java
@@ -0,0 +1,14 @@
+package eu.javageek.bookstore.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Author.class)
+public abstract class Author_ {
+
+	public static volatile SingularAttribute<Author, Integer> id;
+	public static volatile SingularAttribute<Author, String> name;
+
+}
---
+++ Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/domain/Book_.java
@@ -0,0 +1,16 @@
+package eu.javageek.bookstore.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Book.class)
+public abstract class Book_ {
+
+	public static volatile SingularAttribute<Book, Integer> id;
+	public static volatile SingularAttribute<Book, Author> author;
+	public static volatile SingularAttribute<Book, Genre> genre;
+	public static volatile SingularAttribute<Book, String> name;
+	
+}
