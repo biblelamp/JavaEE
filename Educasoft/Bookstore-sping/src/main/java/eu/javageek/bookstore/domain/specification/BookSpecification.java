7c81ee04136f22008c5cf6f78fb3db042c65d80a
Sergey Iryupin <biblelamp@yandex.ru>
2019-01-30 10:49:10 +0100

    added new specification

--- Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/domain/specification/BookSpecification.java
+++ Educasoft/Bookstore-sping/src/main/java/eu/javageek/bookstore/domain/specification/BookSpecification.java
@@ -2,6 +2,7 @@ package eu.javageek.bookstore.domain.specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 import eu.javageek.bookstore.domain.Book;
 import eu.javageek.bookstore.domain.Book_;
+import eu.javageek.bookstore.domain.Genre;
 import eu.javageek.bookstore.domain.Author;
 
 public class BookSpecification {
@@ -30,6 +32,27 @@ public class BookSpecification {
 					final CriteriaBuilder cb) {
 
 				query.where(cb.equal(root.get(Book_.authorOfBook), author));
+				query.distinct(true);
+
+				return query.getRestriction();
+			}
+		};
+	}
+
+	/**
+	 * get {@link Specification} to get all {@link Book} with specified ({@link Genre})
+	 * @param genre
+	 * @return
+	 */
+	public static Specification<Book> getBookByGenre(final Genre genre) {
+		return new Specification<Book>() {
+
+			@Override
+			public Predicate toPredicate(final Root<Book> root, final CriteriaQuery<?> query,
+					final CriteriaBuilder cb) {
+
+				Join<Book, Genre> bookGenre = root.join(Book_.genre);
+				query.where(bookGenre.in(genre));
 				query.distinct(true);
 
 				return query.getRestriction();
