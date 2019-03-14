28a3b74afd8d38245efd6fb6846872d7ed1e2f84
Sergey Iryupin <biblelamp@yandex.ru>
2019-03-14 09:55:06 +0100

    init commit

---
+++ Educasoft/springboot-angularjs/src/main/java/eu/javageek/springbootangularjs/SpringBootAngularJsApplication.java
@@ -0,0 +1,13 @@
+package eu.javageek.springbootangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularJsApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(SpringBootAngularJsApplication.class, args);
+    }
+
+}
