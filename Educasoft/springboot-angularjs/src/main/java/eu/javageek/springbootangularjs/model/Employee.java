18d35789a8d502c734875445a86cbb8091c84558
Sergey Iryupin <biblelamp@yandex.ru>
2019-03-14 10:08:37 +0100

    added model

--- Educasoft/springboot-angularjs/src/main/java/eu/javageek/springbootangularjs/model/Employee.java
+++ Educasoft/springboot-angularjs/src/main/java/eu/javageek/springbootangularjs/model/Employee.java
@@ -6,52 +6,52 @@ public class Employee {
     private String empNo;
     private String empName;
     private String position;
- 
+
     public Employee() {
     }
- 
+
     public Employee(EmployeeForm empForm) {
         this.empId = empForm.getEmpId();
         this.empNo = empForm.getEmpNo();
         this.empName = empForm.getEmpName();
         this.position = empForm.getPosition();
     }
- 
+
     public Employee(Long empId, String empNo, String empName, String position) {
         this.empId = empId;
         this.empNo = empNo;
         this.empName = empName;
         this.position = position;
     }
- 
+
     public Long getEmpId() {
         return empId;
     }
- 
+
     public void setEmpId(Long empId) {
         this.empId = empId;
     }
- 
+
     public String getEmpNo() {
         return empNo;
     }
- 
+
     public void setEmpNo(String empNo) {
         this.empNo = empNo;
     }
- 
+
     public String getEmpName() {
         return empName;
     }
- 
+
     public void setEmpName(String empName) {
         this.empName = empName;
     }
- 
+
     public String getPosition() {
         return position;
     }
- 
+
     public void setPosition(String position) {
         this.position = position;
     }
--- Educasoft/springboot-angularjs/src/main/java/eu/javageek/springbootangularjs/model/EmployeeForm.java
+++ Educasoft/springboot-angularjs/src/main/java/eu/javageek/springbootangularjs/model/EmployeeForm.java
@@ -6,35 +6,35 @@ public class EmployeeForm {
     private String empNo;
     private String empName;
     private String position;
- 
+
     public Long getEmpId() {
         return empId;
     }
- 
+
     public void setEmpId(Long empId) {
         this.empId = empId;
     }
- 
+
     public String getEmpNo() {
         return empNo;
     }
- 
+
     public void setEmpNo(String empNo) {
         this.empNo = empNo;
     }
- 
+
     public String getEmpName() {
         return empName;
     }
- 
+
     public void setEmpName(String empName) {
         this.empName = empName;
     }
- 
+
     public String getPosition() {
         return position;
     }
- 
+
     public void setPosition(String position) {
         this.position = position;
     }
