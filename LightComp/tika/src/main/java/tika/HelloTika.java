c8c0fadd355c727958232992a593cb9990f12abd
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-07 17:21:11 +0100

    init commit

---
+++ LightComp/tika/src/main/java/tika/HelloTika.java
@@ -0,0 +1,13 @@
+package tika;
+
+public class HelloTika {
+
+    public static void main(String[] args) {
+        HelloTika hello = new HelloTika();
+        System.out.println(hello.sayHello());
+    }
+
+    String sayHello() {
+        return "Hello, World!";
+    }
+}
