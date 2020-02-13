f2165d5c63a408110d6738f5c9a9594725be7f14
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-13 11:28:43 +0100

    parsing various date formats

--- LightComp/tika/src/main/java/tika/HelloTika.java
+++ LightComp/tika/src/main/java/tika/HelloTika.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.tika.Tika;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,27 +25,33 @@ public class HelloTika {
 
     private final static Logger log = LoggerFactory.getLogger(HelloTika.class);
 
-    private final static String PATH = "C:\\temp\\upl-ws\\testdata\\MSK_Sada_01\\DOC_67";
+    private final static String PATH = "C:\\temp\\upl-ws\\testdata\\MSK_Sada_01";
     //private final static String PATH = "C:\\temp\\upl-ws\\testdata\\MSK_Sada_01_bad_files";
 
-    public static void main(String[] args) throws IOException {
+    private final static String[] DATE_PARSE_PATTERNS = new String[] { "yyyy:mm:dd", "yyyy-mm-dd", "mm/dd/yy" };
+
+    private static int total = 0;
+    private static int error = 0;
+    private static int empty = 0;
+
+    public static void main(String[] args) throws IOException, java.text.ParseException {
         List<Path> files = Files.walk(Paths.get(PATH))
                 .filter(Files::isRegularFile)
                 .collect(Collectors.toList());
 
         log.info("Start reading");
 
-        Tika tika = new Tika();
+        //Tika tika = new Tika();
 
         ExifTool exifTool = new ExifToolBuilder().withPath("C:\\Program Files\\Java\\jdk1.8\\bin\\exiftool.exe").build();
 
         for (Path p : files) {
-            String mimeType = tika.detect(p);
-            log.info("{}\t{}", mimeType, p);
+            //String mimeType = tika.detect(p);
+            //log.info("{}\t{}", mimeType, p);
 
             Map<Tag, String> metadata = exifTool.getImageMeta(p.toFile());
-            String createDate = modifyDateString(metadata.get(new UnspecifiedTag("CreateDate")));
-            String modifyDate = modifyDateString(metadata.get(new UnspecifiedTag("ModifyDate")));
+            String createDate = metadata.get(new UnspecifiedTag("CreateDate"));
+            String modifyDate = metadata.get(new UnspecifiedTag("ModifyDate"));
             String comments = metadata.get(new UnspecifiedTag("Comments"));
             String keywords = metadata.get(new UnspecifiedTag("Keywords"));
             if (StringUtils.isEmpty(comments)) {
@@ -52,48 +61,88 @@ public class HelloTika {
                 keywords = null;
             }
 
-            System.out.println(createDate);
-            System.out.println(modifyDate);
-            System.out.println(comments);
-            System.out.println(keywords);
+            //System.out.println(createDate);
+            //System.out.println(modifyDate);
+            //System.out.println(comments);
+            //System.out.println(keywords);
+        
+            total += 2;
+        
+            if (createDate == null) {
+                createDate = metadata.get(new UnspecifiedTag("FileCreateDate"));
+            }
+            if (createDate != null) {
+                try {
+                    String modifyCreateDate = modifyDateString(createDate, DATE_PARSE_PATTERNS);
+                    if (modifyCreateDate != null) {
+                        Instant.parse(modifyCreateDate);
+                    }
+                } catch (Exception e) {
+                    log.error("Error parsing date {}", createDate);
+                    error++;
+                }
+            } else {
+                empty++;
+            }
 
-            //for (Entry<Tag, String> entry : metadata.entrySet()) {
-            //    System.out.println(entry.getKey().getName() + ": " + entry.getValue());
-            //}
+            if (modifyDate == null) {
+                modifyDate = metadata.get(new UnspecifiedTag("FileModifyDate"));
+            }
+            if (modifyDate != null) {
+                try {
+                    String modifyModifyDate = modifyDateString(modifyDate, DATE_PARSE_PATTERNS);
+                    if (modifyModifyDate != null) {
+                        Instant.parse(modifyModifyDate);
+                    }
+                } catch (Exception e) {
+                    log.error("Error parsing date {}", modifyDate);
+                    error++;
+                }
+            } else {
+                empty++;
+            }
 
-            /*
-            try (TikaInputStream inputStream = TikaInputStream.get(p)) {
-                BodyContentHandler handler = new BodyContentHandler(-1);
-                Metadata metadataFile = new Metadata();
-                ParseContext context = new ParseContext();
-                Parser parser = new AutoDetectParser();
-                parser.parse(inputStream, handler, metadataFile, context);
-            } catch (Exception e) {
-                log.error("Error reading metadata from {} {}", mimeType, p);
-                e.printStackTrace();
-            }*/
+            //            for (Entry<Tag, String> entry : metadata.entrySet()) {
+            //                System.out.println(entry.getKey().getName() + ": " + entry.getValue());
+            //            }
+
+            //            try (TikaInputStream inputStream = TikaInputStream.get(p)) {
+            //                BodyContentHandler handler = new BodyContentHandler(-1);
+            //                Metadata metadataFile = new Metadata();
+            //                ParseContext context = new ParseContext();
+            //                Parser parser = new AutoDetectParser();
+            //                parser.parse(inputStream, handler, metadataFile, context);
+            //            } catch (Exception e) {
+            //                log.error("Error reading metadata from {} {}", mimeType, p);
+            //                e.printStackTrace();
+            //            }
 
         }
-        log.info("Finish reading");
+        log.info("TOTAL {} ERROR {} NULL {}", total, error, empty);
     }
 
     /**
      * Converting date from exif to standard form like 2020-02-11T09:24:08Z
      * 
-     * @param date like '2020:02:11 09:24:08+01:00'
-     * @return String like '2020-02-11T09:24:08Z' or null
+     * @param date in string
+     * @param parsePatterns list of patters for parsing date
+     * @return String
      */
-    static String modifyDateString(String date) {
+    static String modifyDateString(String date, String[] parsePatterns) {
         if (date == null) {
             return null;
         } else {
-            try {
-                return date.substring(0, 4) + '-' + date.substring(5, 7) + '-' + date.substring(8, 10) +
-                        'T' + date.substring(11, 19) + 'Z';
-            } catch (Exception e) {
-                log.error("Error in date string {}", date);
-                return null;
+            for (final String pattern : parsePatterns) {
+                try {
+                    Date dt = new SimpleDateFormat(pattern).parse(date);
+                    return String.format("%tY-%tm-%tdT00:00:00Z", dt, dt, dt);
+                } catch (ParseException ignore) {
+                    // the pattern didn't fit this date
+                }
             }
+            error++;
+            log.error("Error in date string {}", date);
+            return null;
         }
     }
 
