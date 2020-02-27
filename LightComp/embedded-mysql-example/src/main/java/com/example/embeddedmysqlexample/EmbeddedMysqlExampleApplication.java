dcf7f729ce9f4e2dbc4731165cd8ad6f1d5ff2f7
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-27 16:26:41 +0100

    init commit

---
+++ LightComp/embedded-mysql-example/src/main/java/com/example/embeddedmysqlexample/EmbeddedMysqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.embeddedmysqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedMysqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedMysqlExampleApplication.class, args);
+	}
+
+}
---
+++ LightComp/embedded-mysql-example/src/test/java/com/example/embeddedmysqlexample/EmbeddedMysqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embeddedmysqlexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedMysqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
