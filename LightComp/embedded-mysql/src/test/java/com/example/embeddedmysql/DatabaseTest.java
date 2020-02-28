3fdec5561bbabc7fafd59fead8d4d2dd3043da89
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-28 11:37:43 +0100

    decrease spring-boot version until 2.1.4

--- LightComp/embedded-mysql/src/test/java/com/example/embeddedmysql/DatabaseTest.java
+++ LightComp/embedded-mysql/src/test/java/com/example/embeddedmysql/DatabaseTest.java
@@ -8,17 +8,14 @@ import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class DatabaseTest {
