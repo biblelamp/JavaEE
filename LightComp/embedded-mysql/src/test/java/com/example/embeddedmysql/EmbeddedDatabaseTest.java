9ebc50cf1029d868e3b08f4c4a4de6f6dacd4e10
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-28 17:40:59 +0100

    added test class

---
+++ LightComp/embedded-mysql/src/test/java/com/example/embeddedmysql/EmbeddedDatabaseTest.java
@@ -0,0 +1,90 @@
+package com.example.embeddedmysql;
+
+import static com.wix.mysql.distribution.Version.v5_7_19;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.MysqldConfig;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+public class EmbeddedDatabaseTest {
+
+    private final static Logger log = LoggerFactory.getLogger(EmbeddedDatabaseTest.class);
+
+    private static EmbeddedMysql embeddedMysql;
+
+    private static Connection connection;
+
+    @BeforeAll
+    public static void init() throws SQLException, LiquibaseException {
+        MysqldConfig config = MysqldConfig.aMysqldConfig(v5_7_19)
+                .withPort(3307)
+                .withTimeZone(TimeZone.getTimeZone(ZoneId.of("UTC")))
+                .withUser("test", "test")
+                .build();
+
+        embeddedMysql = EmbeddedMysql.anEmbeddedMysql(config)
+                .addSchema("test_database")
+                .start();
+
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+
+        dataSourceBuilder.username(embeddedMysql.getConfig().getUsername());
+        dataSourceBuilder.password(embeddedMysql.getConfig().getPassword());
+        dataSourceBuilder.driverClassName(com.mysql.cj.jdbc.Driver.class.getName());
+        dataSourceBuilder.url("jdbc:mysql://localhost:3307/test_database");
+
+        DataSource dataSource = dataSourceBuilder.build();
+
+        connection = dataSource.getConnection();
+
+        Database database = DatabaseFactory
+                .getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(dataSource.getConnection()));
+
+        Liquibase liquibase = new Liquibase("db/db.changelog-1.0.xml",
+                new ClassLoaderResourceAccessor(),
+                database);
+
+        liquibase.update(new Contexts());
+
+    }
+
+    @AfterAll
+    public static void stop() {
+        if (null != embeddedMysql) {
+            embeddedMysql.stop();
+        }
+    }
+
+    @Test
+    public void testSelect() throws SQLException {
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT 1");
+        assertEquals(true, true);
+    }
+
+}
