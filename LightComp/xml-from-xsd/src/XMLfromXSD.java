5d9e5a1a7a3b52c30b7e975b7c1fb276cadafeaf
Sergey Iryupin <biblelamp@yandex.ru>
2020-05-25 16:32:01 +0200

    xsd to xml

---
+++ LightComp/xml-from-xsd/src/XMLfromXSD.java
@@ -0,0 +1,42 @@
+// from https://www.javacodegeeks.com/2011/02/jaxb-generate-xml-xsd.html
+// generate classes from xsd file:
+// xjc.exe expense.xsd
+
+import generated.ItemT;
+import generated.UserT;
+import generated.ObjectFactory;
+import generated.ItemListT;
+import generated.ExpenseT;
+
+import java.math.BigDecimal;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class XMLfromXSD {
+    public static void main(String[] args) throws JAXBException {
+        ObjectFactory factory = new ObjectFactory();
+
+        UserT user = factory.createUserT();
+        user.setUserName("Sergey");
+        ItemT item = factory.createItemT();
+        item.setItemName("Seagate External HDD");
+        item.setPurchasedOn("August 24, 2010");
+        item.setAmount(new BigDecimal("6776.5"));
+
+        ItemListT itemList = factory.createItemListT();
+        itemList.getItem().add(item);
+
+        ExpenseT expense = factory.createExpenseT();
+        expense.setUser(user);
+        expense.setItems(itemList);
+
+        JAXBContext context = JAXBContext.newInstance("generated");
+        JAXBElement<ExpenseT> element = factory.createExpenseReport(expense);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty("jaxb.formatted.output", Boolean.TRUE);
+        marshaller.marshal(element, System.out);
+    }
+}
