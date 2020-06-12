9530f064ca023c4447a1b7932ecb1cd663f849e5
Sergey Iryupin <biblelamp@yandex.ru>
2020-06-12 17:54:47 +0200

    using @EnableScheduling and @Scheduled()

--- LightComp/spring-boot/src/main/java/io/springboot/hello/HelloApplication.java
+++ LightComp/spring-boot/src/main/java/io/springboot/hello/HelloApplication.java
@@ -2,7 +2,9 @@ package io.springboot.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class HelloApplication {
 
--- LightComp/spring-boot/src/main/java/io/springboot/hello/HelloService.java
+++ LightComp/spring-boot/src/main/java/io/springboot/hello/HelloService.java
@@ -1,10 +1,19 @@
 package io.springboot.hello;
 
 import org.springframework.context.SmartLifecycle;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class HelloService implements SmartLifecycle {
+
+    @Scheduled(fixedRate = 5000)
+    public void sheduled() {
+        System.out.printf("%tc Sheduled: Hello, world!\n", new Date());
+    }
 
     @Override
     public void start() {
