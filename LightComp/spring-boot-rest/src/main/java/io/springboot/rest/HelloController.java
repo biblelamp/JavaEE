5d59cd599729fd08ecfcdbc5efb95cbf79b8aade
Sergey Iryupin <biblelamp@yandex.ru>
2020-06-18 10:20:40 +0200

    simple hello controller

---
+++ LightComp/spring-boot-rest/src/main/java/io/springboot/rest/HelloController.java
@@ -0,0 +1,14 @@
+package io.springboot.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+
+}
