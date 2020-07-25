6feeb77cf9f8648c38a633be15a2977e1cdea95f
Sergey Iryupin <biblelamp@yandex.ru>
2020-07-25 15:04:06 +0200

    added exec-maven-plugin

--- LightComp/rtf-parser-kit/src/main/java/rtf/parserkit/Application.java
+++ LightComp/rtf-parser-kit/src/main/java/rtf/parserkit/Application.java
@@ -2,6 +2,6 @@ package rtf.parserkit;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Maven!");
     }
 }
