d9e9e4708377a2fbb567366990c352943f84ae66
Sergey Iryupin <biblelamp@yandex.ru>
2020-07-26 16:29:34 +0200

    extracting text from rtf

--- LightComp/rtf-parser-kit/src/main/java/rtf/parserkit/Application.java
+++ LightComp/rtf-parser-kit/src/main/java/rtf/parserkit/Application.java
@@ -1,7 +1,18 @@
 package rtf.parserkit;
 
+import com.rtfparserkit.converter.text.StringTextConverter;
+import com.rtfparserkit.parser.RtfStreamSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+    public static void main(String[] args) throws IOException {
+
+        StringTextConverter converter = new StringTextConverter();
+        converter.convert(new RtfStreamSource(new FileInputStream("src/main/resources/testfile.rtf")));
+        String extractedText = converter.getText();
+
+        System.out.println(extractedText);
     }
 }
