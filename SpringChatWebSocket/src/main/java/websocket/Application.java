9925ea633469e4e1174a6a45b8c1b374b53b7cbd
Sergey Iryupin <biblelamp@yandex.ru>
2022-02-19 09:10:51 +0100

    spring boot websocket chat

---
+++ SpringChatWebSocket/src/main/java/websocket/Application.java
@@ -0,0 +1,13 @@
+package websocket;
+
+// From: https://sysout.ru/chat-na-spring-boot-i-websocket/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
