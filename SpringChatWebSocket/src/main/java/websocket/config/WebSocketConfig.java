224282da5729e20d8f3c2150ce0b2ddcb91e6d1f
Sergey Iryupin <biblelamp@yandex.ru>
2022-04-17 15:33:04 +0200

    enable sheduling

--- SpringChatWebSocket/src/main/java/websocket/config/WebSocketConfig.java
+++ SpringChatWebSocket/src/main/java/websocket/config/WebSocketConfig.java
@@ -2,11 +2,13 @@ package websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableScheduling
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
