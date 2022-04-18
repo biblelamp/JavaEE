fbf585ab10f1c2e31b7f4abf46ae1e840c865ed3
Sergey Iryupin <biblelamp@yandex.ru>
2022-04-18 15:35:24 +0200

    sending scheduled messages

--- SpringChatWebSocket/src/main/java/websocket/controller/SheduledMessages.java
+++ SpringChatWebSocket/src/main/java/websocket/controller/SheduledMessages.java
@@ -2,16 +2,27 @@ package websocket.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import websocket.model.ChatMessage;
 
 @Component
-public class SheduledMessage {
+public class SheduledMessages {
 
-    private static final Logger logger = LoggerFactory.getLogger(SheduledMessage.class);
+    private static final Logger logger = LoggerFactory.getLogger(SheduledMessages.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
 
     @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
         logger.info("Fixed delay task - {}", System.currentTimeMillis() / 1000);
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setType(ChatMessage.MessageType.CHAT);
+        chatMessage.setSender("timer");
+        chatMessage.setContent(String.valueOf(System.currentTimeMillis() / 1000));
+        messagingTemplate.convertAndSend("/topic/public", chatMessage);
     }
 }
