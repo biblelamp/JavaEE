cc485274bfdb7292e848f599d4716903d8849cfa
Sergey Iryupin <biblelamp@yandex.ru>
2023-03-25 18:43:45 +0100

    added files created by https://earth.google.com/

--- HelloKml/src/main/java/GtXsdKmlDemo.java
+++ HelloKml/src/main/java/GtXsdKmlDemo.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * from https://gis.stackexchange.com/questions/4549/how-to-parse-kml-data-using-geotools
@@ -17,19 +18,38 @@ import java.io.InputStream;
  */
 
 public class GtXsdKmlDemo {
-    static String kmlFile = "KML-Orlik-19012023-hz6d6xbb.kml";
+    // file created by https://www.freemaptools.com/kml-file-creator.htm
+    static String kmlOrlik = "KML-Orlik-POINT.kml";
+
+    // files created by https://earth.google.com/
+    static String kmlLondon = "KML-London-POINT.kml";
+    static String kmlPragRomeLine = "KML-Prag-Rome-LINE.kml";
+    static String kmlPrahuePolygon = "KML-Prague-POLYGON.kml";
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
-        try (InputStream is = new FileInputStream(kmlFile)) {
+        try (InputStream is = new FileInputStream(kmlPrahuePolygon)) {
             Parser parser = new Parser(new KMLConfiguration());
-            SimpleFeature sf = (SimpleFeature) parser.parse(is);
-            for (Property property : sf.getValue()) {
-                System.out.println(property);
-                System.out.println(property.getType());
-                System.out.println(property.getValue());
+            SimpleFeature simpleFeature = (SimpleFeature) parser.parse(is);
+            for (Property property : simpleFeature.getValue()) {
+                System.out.println("p: " + property);
+                System.out.println("t: " + property.getType());
+                System.out.println("v: " + property.getValue());
                 if (property.getType().getName().getURI().equals(KML.Geometry.getLocalPart())) {
-                    System.out.println(property.getValue());
+                    System.out.println("result:\n" + property.getValue());
+                }
+                if (property.getType().getName().getURI().equals(KML.Feature.getLocalPart())) {
+                    List<SimpleFeature> simpleFeatures = (List<SimpleFeature>) property.getValue();
+                    for (SimpleFeature sf : simpleFeatures) {
+                        for (Property p : sf.getValue()) {
+                            System.out.println("p: " + p);
+                            System.out.println("t: " + p.getType());
+                            System.out.println("v: " + p.getValue());
+                            if (p.getType().getName().getURI().equals(KML.Geometry.getLocalPart())) {
+                                System.out.println("result:\n" + p.getValue());
+                            }
+                        }
+                    }
                 }
             }
         }
